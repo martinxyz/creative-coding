6845a500632752803efd500498f6e62ac63f0446
Martin Renold <martin@log2.ch>
2021-05-16 08:00:46 +0200

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ fn main() {
     nannou::app(model).update(update).run();
 }
 
-const N: usize = 20000;
-
 struct State {
     x: f32,
     r: f32,
@@ -14,13 +12,20 @@ struct State {
 
 impl State {
     fn init() -> Self {
-        return Self {
+        Self {
             x: random_f32(),
             r: random_f32() * 4.0 + 0.5,
             iteration: 0,
         }
     }
+    fn step(&mut self) {
+        // logistic map formula
+        self.x = self.r * self.x * (1.0 - self.x);
+        self.iteration += 1;
+    }
 }
+
+const N: usize = 20_000;
 
 struct Model {
     _window: window::Id,
@@ -37,19 +42,18 @@ fn model(app: &App) -> Model {
 
     Model {
         _window,
-        states: (0..N).map(|_| State::init()).collect()
+        states: (0..N).map(|_| State::init()).collect(),
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
     let prob = 1. - sigmoid((app.elapsed_frames() as f32 + 500.) as f32 / 500.);
-    println!("{} prob: {:?}", app.elapsed_frames(), prob);
+    println!("frame {} reset probability {:?}", app.elapsed_frames(), prob);
     for state in model.states.iter_mut() {
         if random_f32() < prob {
             *state = State::init();
         } else {
-            state.x = state.r * state.x * (1.0 - state.x);
-            state.iteration += 1;
+            state.step()
         }
     }
 }
@@ -60,40 +64,30 @@ fn view(app: &App, model: &Model, frame: Frame) {
     const SCALE_X: f32 = 1024.0 * 1.4;
     const SCALE_Y: f32 = 1024.0 * 0.4;
 
-    if app.elapsed_frames() < 2 {
-        // draw.background().color(PLUM);
+    if app.elapsed_frames() == 1 {
         draw.background().color(WHITE);
-    } else {
-        // draw.background().rgba(1.0, 1.0, 1.0, 0.001).;
-        // draw.alpha_blend(BLEND_NORMAL).background().rgba(1.0, 1.0, 1.0, 0.001);
-        // draw.rect()
-        //     .rgba(1.0, 1.0, 1.0, 0.002)
-        //     .x(0.2 * SCALE_X)
-        //     .y(-0.15 * SCALE_Y)
-        //     .width(0.15 * SCALE_X)
-        //     .height(0.4 * SCALE_Y)
-        //     ;
     }
 
-    let colorfac = sigmoid((500. - app.elapsed_frames() as f32) as f32 / 1500.);
+    let color_fac = sigmoid((500. - app.elapsed_frames() as f32) / 1500.);
 
     for state in model.states.iter() {
         let x = state.r / 5.0;
         let y = state.x;
-        let it_fac = 1.0 / (state.iteration as f32 + 2.) + 1./2.;
-        draw
-            // .blend(BLEND_DARKEST)
-            // .point_mode()
-            .ellipse()
-            .rgba(colorfac * 0.9, colorfac * 0.3, colorfac * 0.2, (1.0 - it_fac) * 0.5)
-            .x(x * SCALE_X - SCALE_X/2.0)
-            .y(y * SCALE_Y - SCALE_Y/2.0)
-            .radius(0.05 + it_fac * 0.0)
-            ;
+        let it_fac = 1.0 / (state.iteration as f32 + 2.) + 0.5;
+        draw.ellipse()
+            .rgba(
+                color_fac * 0.9,
+                color_fac * 0.3,
+                color_fac * 0.2,
+                (1.0 - it_fac) * 0.5,
+            )
+            .x(x * SCALE_X - SCALE_X / 2.0)
+            .y(y * SCALE_Y - SCALE_Y / 2.0)
+            .radius(0.05 + it_fac * 0.0);
     }
     draw.to_frame(app, &frame).unwrap();
 }
 
 fn sigmoid(x: f32) -> f32 {
-    return 1.0 / (1.0 + (-x).exp());
+    1.0 / (1.0 + (-x).exp())
 }
